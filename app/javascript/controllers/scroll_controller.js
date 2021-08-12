0e2aad16cde00baf900fe4c3fa7f821b47f722f6
Stewie12061 <nhut170501@gmail.com>
2021-08-12 17:17:52 +0700

    Revert "Revert "stimulus""
    This reverts commit 7789fb073345b68b323e48b25a23fec3e17118ba.

---
+++ app/javascript/controllers/scroll_controller.js
@@ -0,0 +1,52 @@
+import ApplicationController from './application_controller'
+import lozad from 'lozad'
+
+export default class extends ApplicationController {
+  static targets = ['primary']
+
+  connect() {
+    super.connect()
+    this.onScroll();
+  }
+
+  initialize(){
+    document.addEventListener('cable-ready:after-morph', this.reconnect.bind(this))
+  }
+  reconnect(){
+    this.onScroll();
+  }
+  onScroll(){
+    let self = this
+    self.showTopBtn()
+    window.addEventListener("scroll", function () {
+      // when position > 117 will fix navbar
+    self.showTopBtn()
+    })
+  }
+
+  showTopBtn() {
+    let scrollY = window.scrollY;
+    if (scrollY >= 800) {
+      document.getElementById("go-top").classList.remove('d-none');
+    }
+
+    if (scrollY == 0) {
+      document.getElementById("go-top").classList.add('d-none');
+    }
+    this.navbarWillFixed(scrollY)
+  }
+
+  gotoTop(event) {
+    event.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  navbarWillFixed(scrollY) {
+    let classes = document.querySelector("#navbar_main").classList;
+    if (scrollY >= 120) {
+      classes.add('navbar-fixed');
+    } else {
+      classes.remove('navbar-fixed');
+    }
+  }
+}
